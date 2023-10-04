e498089ccfc30cf5e58754b5e99252d3a50e00e3
viniciussilvaribeiro <130010792+viniciussilvaribeiro@users.noreply.github.com>
2023-10-04 17:23:25 -0300

    Update db_utils.py

--- persistence/db_utils.py
+++ persistence/db_utils.py
@@ -3,6 +3,7 @@ from sqlmodel import create_engine, SQLModel
 
 def get_engine():
     url = 'sqlite:///database.db'
+    url = 'postgresql+psycopg2://postgres:tasks_senha123V@db.lfhqvmxbrworgpptumfe.supabase.co:5432/postgres'
     return create_engine(url, echo=True)
 
 
